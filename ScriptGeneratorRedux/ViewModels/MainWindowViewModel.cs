e00afe78a17ffac5e352e6485800adf28ee51b60
snwmelt <tfsmasamvi@gmail.com>
2019-06-21 14:57:03 +0100

    Restructure for MVVM:
    Created Approprite Sub-Directories
    Moved MainWindow location and references
    Imported Walkways Library
    Imported System.ValueTupple as Walkways DEP
    Implemented NavigationSerivice provider stub ( for future use? )

---
+++ ScriptGeneratorRedux/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using Walkways.MVVM.View_Model;
+
+namespace ScriptGeneratorRedux.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Private Variables
+
+        private Page        _CurrentView;
+        private INPCInvoker _INPCInvoke;
+
+        #endregion
+
+        public MainWindowViewModel( )
+        {
+            _INPCInvoke = new INPCInvoker( this );
+        }
+
+        public Page CurrentView
+        {
+            get
+            {
+                return _CurrentView;
+            }
+
+            set
+            {
+                _INPCInvoke.AssignPropertyValue<Page>( ref PropertyChanged, ref _CurrentView, value );
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ScriptGeneratorRedux/Views/MainWindowView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ScriptGeneratorRedux.Views.Interfaces;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace ScriptGeneratorRedux.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window, INavigationServiceProvider
+    {
+        public MainWindowView( )
+        {
+            InitializeComponent( );
+        }
+
+        public NavigationService NavigationService
+        {
+            get
+            {
+                return MainWindowContentFrame.NavigationService;
+            }
+        }
+    }
+}
