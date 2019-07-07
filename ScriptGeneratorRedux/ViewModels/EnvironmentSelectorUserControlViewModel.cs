0b9bd4302131fcbd01725cc2e186f9ae03fe8a1d
snwmelt <ekitai.k@gmail.com>
2019-07-07 16:58:13 +0100

    Initial Bindings For Environmental Selector
    Stub for implementing binding to environment selector view model

---
+++ ScriptGeneratorRedux/ViewModels/EnvironmentSelectorUserControlViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace ScriptGeneratorRedux.ViewModels
+{
+    internal class EnvironmentSelectorUserControlViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
