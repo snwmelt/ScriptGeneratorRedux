4c6468cb37e3cd3017b028d48c25834a274b49f1
snwmelt <ekitai.k@gmail.com>
2019-07-22 19:47:26 +0100

    Added TOML Language Support
    Added TOML langauge grammar parser and lexer for use in creation and loading of configuration files.

---
+++ SGRAntlr/Grammars/Parsers/tomlParser.g4.cs
@@ -0,0 +1,8 @@
+﻿namespace SGRAntlrl.Grammars.Parsers
+{
+#pragma warning disable CS3021 // Type or member does not need a CLSCompliant attribute because the assembly does not have a CLSCompliant attribute
+    partial class tomlParser
+#pragma warning restore CS3021 // Type or member does not need a CLSCompliant attribute because the assembly does not have a CLSCompliant attribute
+    {
+    }
+}
