7a3d56fe62d57c1cc887529496e686c9a2323312
snwmelt <ekitai.k@gmail.com>
2019-07-22 21:45:53 +0100

    Added SGRAntlr Resource Factory
    Added SGRAntlr ResourceFactory class, SGRAntlrResourceProvider, and IResourceProvider Interface to allow easy instanciation of antler input streams, contexts, parsers and lexers.

---
+++ SGRAntlr/Helpers/Interfaces/IResourceProvider`2.cs
@@ -0,0 +1,17 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System;
+
+namespace SGRAntlrl.Helpers.Interfaces
+{
+    internal interface IResourceProvider<TLexer, TParser>  
+        where TLexer : Lexer
+        where TParser : Parser
+    {
+        TLexer Lexer { get; }
+        TParser Parser { get; }
+        void SetSourceText( String text );
+        CommonTokenStream TokenStream { get; }
+        void Walk( IParseTreeListener ParseTreeListener, IParseTree ParseTree );
+    }
+}
---
+++ SGRAntlr/Helpers/ResourceFactory.cs
@@ -0,0 +1,15 @@
+﻿using SGRAntlrl.Helpers.Interfaces;
+using System;
+
+namespace SGRAntlrl.Helpers
+{
+    internal sealed class ResourceFactory
+    {
+        internal static IResourceProvider<T1, T2> GetResourceProvider<T1, T2>( String InputText = "" )
+            where T1 : Antlr4.Runtime.Lexer
+            where T2 : Antlr4.Runtime.Parser
+        {
+            return new SGRAntlrResourceProvider<T1, T2>( );
+        }
+    }
+}
