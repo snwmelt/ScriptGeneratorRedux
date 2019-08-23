0c26bd5464727ae24e0bada72a52962b56aaa16c
snwmelt <ekitai.k@gmail.com>
2019-08-23 13:43:09 +0100

    Adding ICP4SecurityServer interface
    Added supporting interfaces for initial draft of CP4Security DB definition and access.

---
+++ ScriptGeneratorRedux/Models/Core/IO/CP4DBO/Interfaces/ICP4SecurityServer.cs
@@ -0,0 +1,9 @@
+﻿using ScriptGeneratorRedux.Models.Core.IO.Database.Interfaces;
+
+namespace ScriptGeneratorRedux.Models.Core.IO.CP4DBO.Interfaces
+{
+    internal interface ICP4SecurityServer : ISQLServer
+    {
+        ISQLDatabase SecurityDB { get; }
+    }
+}
---
+++ ScriptGeneratorRedux/Models/Core/IO/Database/Interfaces/ISQLDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ScriptGeneratorRedux.Models.Core.IO.Database.Interfaces
+{
+    internal interface ISQLDatabase
+    {
+        ISQLServer Server { get; }
+        String Name { get; }
+        IDictionary<ISQLTabelKey, IEnumerable<dynamic>> Data { get; }
+    }
+}
