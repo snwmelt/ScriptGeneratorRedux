cbba4f568e365ee49cda84514f5b0edbe22d8527
snwmelt <ekitai.k@gmail.com>
2019-08-23 13:30:10 +0100

    Expanding ICP4DatabaseServiceProvider
    Added GetEnvironments and GetStudyIDs methods to the ICP4DatabaseServiceProvider interface
    Added ID Property to ICP4 Interface
    Added Calls to resolve data fetching via Core ICP4DatabaseServiceProvider instance to DataContext.

--- ScriptGeneratorRedux/Models/Core/IO/CP4DBO/Interfaces/ICP4Study.cs
+++ ScriptGeneratorRedux/Models/Core/IO/CP4DBO/Interfaces/ICP4Study.cs
@@ -1,9 +1,11 @@
 ﻿using ScriptGeneratorRedux.Models.Core.IO.CP4DBO.Enums;
 using ScriptGeneratorRedux.Models.Core.IO.Interfaces;
+using System;
 
 namespace ScriptGeneratorRedux.Models.Core.IO.CP4DBO.Interfaces
 {
     internal interface ICP4Study : IEnumerableDataSource<ECP4DepoplymentEnvironment>
     {
+        Int64 ID { get; }
     }
 }
--- ScriptGeneratorRedux/Models/Core/IO/Database/Interfaces/ICP4DatabaseServiceProvider.cs
+++ ScriptGeneratorRedux/Models/Core/IO/Database/Interfaces/ICP4DatabaseServiceProvider.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using ScriptGeneratorRedux.Models.Core.IO.CP4DBO.Interfaces;
 using System;
+using ScriptGeneratorRedux.Models.Core.IO.CP4DBO.Enums;
 
 namespace ScriptGeneratorRedux.Models.Core.IO.Database.Interfaces
 {
     internal interface ICP4DatabaseServiceProvider
     {
         IReadOnlyCollection<ICP4Study> GetStudies( ISQLServer Server );
+        IEnumerable<ECP4DepoplymentEnvironment> GetEnvironments( ICP4StudyServer iCP4StudyServer );
+        IEnumerable<ICP4Study> GetStudyIDs( String serverName, String EnvironmentName = null );
     }
 }
