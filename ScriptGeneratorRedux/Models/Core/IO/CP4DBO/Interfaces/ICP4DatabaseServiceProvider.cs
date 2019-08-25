0467cb19fb74186006065610cdeaab1729ebe281
snwmelt <ekitai.k@gmail.com>
2019-08-25 17:03:33 +0100

    Update ICP4DatabaseServiceProvider.cs
    Added more methods for fetching StudyIDs to ICP4DatabaseServiceProvider in preperation for a re-write of IDataContext interface.

--- ScriptGeneratorRedux/Models/Core/IO/CP4DBO/Interfaces/ICP4DatabaseServiceProvider.cs
+++ ScriptGeneratorRedux/Models/Core/IO/CP4DBO/Interfaces/ICP4DatabaseServiceProvider.cs
@@ -10,5 +10,9 @@ namespace ScriptGeneratorRedux.Models.Core.IO.CP4DBO.Interfaces
         IReadOnlyCollection<ICP4Study> GetStudies( ISQLServer Server );
         IEnumerable<ECP4DepoplymentEnvironment> GetEnvironments( ICP4StudyServer iCP4StudyServer );
         IEnumerable<ICP4Study> GetStudyIDs( String serverName, String EnvironmentName = null );
+        IEnumerable<long> GetStudyIDs( IEnumerable<ICP4SecurityServer> SecurityServers );
+        IEnumerable<long> GetStudyIDs( ICP4SecurityServer SecurityServer );
+        IEnumerable<long> GetStudyIDs( IEnumerable<ICP4SecurityServer> SecurityServers, ECP4DepoplymentEnvironment Environment );
+        IEnumerable<long> GetStudyIDs( ICP4SecurityServer SecurityServer, ECP4DepoplymentEnvironment Environment );
     }
 }
